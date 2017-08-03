8e63ea3404b245c3350f7577a3e001a0bf6a7d09
Vladimir Rybalka <vladimir@Vladimirs-Mac.local>
2017-08-03 13:53:31 +0300

    Created simple table

--- Simple Table/ViewController.swift
+++ Simple Table/ViewController.swift
@@ -8,7 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITextViewDelegate {
+    
+    private let dwarves = [
+        "Соня", "Чихоня", "Робкий", "Счастливчик", "Док", "Сердитый", "Кумар", "Торин", "Дорин", "Нори", "Ори", "Балин", "Двалин", "Фили", "Кили", "Оин", "Глоин", "Бифур", "Бофур", "Бомбур"
+    ]
+    let simpleTableIdentifier = "SimpleTableIdentifier"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +24,21 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // MARK: Table Data Source
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dwarves.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
+        
+        if(cell == nil) {
+            cell = UITableViewCell(style: .default, reuseIdentifier: simpleTableIdentifier)
+        }
+        cell!.textLabel?.text = dwarves[indexPath.row]
+        
+        return cell!
+    }
 }
 
