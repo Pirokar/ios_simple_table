4f6952910687559c927986502b67326b1bab69c5
Vladimir Rybalka <vladimir@Vladimirs-Mac.local>
2017-08-03 18:01:32 +0300

    Realised simple table with picture and subtitle

--- Simple Table/ViewController.swift
+++ Simple Table/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource, UITextViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     private let dwarves = [
         "Соня", "Чихоня", "Робкий", "Счастливчик", "Док", "Сердитый", "Кумар", "Торин", "Дорин", "Нори", "Ори", "Балин", "Двалин", "Фили", "Кили", "Оин", "Глоин", "Бифур", "Бофур", "Бомбур"
@@ -30,15 +30,53 @@ class ViewController: UIViewController, UITableViewDataSource, UITextViewDelegat
         return dwarves.count
     }
     
+    // MARK: Table Data Delegate
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: simpleTableIdentifier)
         
         if(cell == nil) {
-            cell = UITableViewCell(style: .default, reuseIdentifier: simpleTableIdentifier)
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: simpleTableIdentifier)
         }
+        
+        cell!.imageView?.image = #imageLiteral(resourceName: "star")
+        cell?.imageView?.highlightedImage = #imageLiteral(resourceName: "star2")
+        
+        if(indexPath.row < 7) {
+            cell?.detailTextLabel?.text = "Mr Disney"
+        } else {
+            cell?.detailTextLabel?.text = "Mr Tolkien"
+        }
+        
         cell!.textLabel?.text = dwarves[indexPath.row]
+        cell!.textLabel?.font = UIFont .boldSystemFont(ofSize: 50)
         
         return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, indentationLevelForRowAt indexPath: IndexPath) -> Int {
+        return indexPath.row % 4
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        if(indexPath.row == 0) {
+            return nil
+        } else {
+            return indexPath
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let rowValue = dwarves[indexPath.row]
+        let message = "Вы выбрали \(rowValue)"
+        
+        let controller = UIAlertController(title: "Выбрана строка", message: message, preferredStyle: .alert)
+        let action = UIAlertAction(title: "А то", style: .default, handler: nil)
+        controller.addAction(action)
+        present(controller, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.row == 0 ? 120 : 70
     }
 }
 
